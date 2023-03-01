3f2de6644f8a499aa4003ec2f55fc403bb021861
D N <danuser2018@gmail.com>
2023-03-01 20:51:15 +0100

    Fix BlockchainApplicationIT properties

--- main-application/src/test/java/me/dserrano/blockchain/BlockchainApplicationIT.java
+++ main-application/src/test/java/me/dserrano/blockchain/BlockchainApplicationIT.java
@@ -23,6 +23,18 @@ class BlockchainApplicationIT {
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+        registry.add("blockchain.nodes.self.id", () ->  "self-id");
+        registry.add("blockchain.nodes.self.host", () ->  "localhost");
+        registry.add("blockchain.nodes.self.port", () ->  8080);
+        registry.add("blockchain.nodes.self.publish-rate-ms", () -> 60000);
+        registry.add("blockchain.nodes.topic.name", () -> "node-topic");
+        registry.add("spring.kafka.producer.key-serializer", () -> "org.apache.kafka.common.serialization.StringSerializer");
+        registry.add("spring.kafka.producer.value-serializer", () -> "org.springframework.kafka.support.serializer.JsonSerializer");
+        registry.add("spring.kafka.consumer.group-id", () -> "group-id");
+        registry.add("spring.kafka.consumer.auto-offset-reset", () -> "earliest");
+        registry.add("spring.kafka.consumer.key-deserializer", () -> "org.apache.kafka.common.serialization.StringDeserializer");
+        registry.add("spring.kafka.consumer.value-deserializer", () -> "org.springframework.kafka.support.serializer.JsonDeserializer");
+        registry.add("spring.kafka.consumer.properties.spring.json.trusted.packages", () -> "*");
     }
 
     @Test
